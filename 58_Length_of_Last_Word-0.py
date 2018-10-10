76d2d1054eb8d166d4926222baa9c1380a40d1f2
evelyn <c62066206@hotmail.com>
2018-10-10 15:46:51 +0800

    finished, beat 99.84%

--- 58_Length_of_Last_Word-0.py
+++ 58_Length_of_Last_Word-0.py
@@ -5,8 +5,11 @@ class Solution(object):
         :rtype: int
         """
         words=s.split()
-        if len(words):
-            return len(words[-1])
-        else:
-            return 0
-
+        #if len(words):
+        #    return len(words[-1])
+        #else:
+        #    return 0
+        return len("".join(words[-1:]))
+        # it is faster than if/else
+        # use [-1:] to prevent out of list
+        # use "".join to prevent wrong length
