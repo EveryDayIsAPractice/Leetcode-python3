6c9e0333e182a7ab98e72c59b46068cdcec99357
evelyn <c62066206@hotmail.com>
2018-10-10 16:04:30 +0800

    finished, beat 100%git add 66_Plus_One-0.py <3

---
+++ 66_Plus_One-0.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        if len(digits):
+            if digits[-1]!=9:
+                digits[-1]+=1
+                return digits
+            else:
+                digits[:-1] = self.plusOne(digits[:-1])
+                digits[-1]=0
+                return digits
+        else:
+            return [1]
